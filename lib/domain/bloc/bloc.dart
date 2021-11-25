3e501424902636e1c251e964cfd0e4fdd3ed49cd
Angie Valera <advalera@uninorte.edu.co>
2021-11-25 08:43:39 -0500

    agregando solucion

--- lib/domain/bloc/bloc.dart
+++ lib/domain/bloc/bloc.dart
@@ -19,17 +19,33 @@ class ConvertionBloc extends Bloc<ConvertionEvent, AbstractConverterState> {
     if (event is UpdateDecimalEvent) {
       // Implementa la respuesta al evento como un estado 
       //para convertir de decimal a binario
+
+      _decimal = Converter.adjustValue(_decimal, event.digit);
+      _binary = Converter.dec2bin(_decimal);
+
+      yield ConverterState(_decimal, _binary);
+
     }
 
     if (event is UpdateBinaryEvent) {
       // Implementa la respuesta al evento como un estado 
       //para convertir de binario a decimal
+
+      _binary = Converter.adjustValue(_binary, event.digit);
+      _decimal = Converter.bin2dec(_binary);
+
+      yield ConverterState(_decimal, _binary);
       
     }
 
     if (event is ResetEvent) {
       // Implementa la respuesta al evento como un estado 
       //para resetear la conversión
+
+      _decimal = "0";
+      _binary = "0";
+
+      yield ConverterState(_decimal, _binary);
     
     }
   }
